7bc0eb0052b342e37edb060ab0f01e9fcceabd78
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-01-16 15:20:45 -0500

    adds color and box to homepage

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,21 @@
 
 import './styles/App.css';
+import {Container, Box, Grid, Typography} from '@mui/material'
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <Container maxWidth='lg'>
+      <Box className='content'>
+        <Grid container spacing={2}>
+          <Grid item xs={12} className='header'>
+            <Typography>
+              Testing
+            </Typography>
+
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
 
