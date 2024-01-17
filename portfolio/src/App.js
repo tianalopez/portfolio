56d158fc4e2e1caa2e37c6a8448442609a09492d
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-01-17 11:22:15 -0500

    fixes outlet

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
 
 import './styles/App.css';
-import {Container, Box, Grid, Typography, Link} from '@mui/material';
-import { NavLink } from "react-router-dom";
+import {Container, Box, Grid, Typography} from '@mui/material';
+import { NavLink, Outlet } from "react-router-dom";
 
 function App() {
 
@@ -48,14 +48,7 @@ function App() {
             <Typography sx={{ml:1.5}}>•••</Typography>
           </Grid>
         </Grid>
-        <Grid
-          className='flex-content' container spacing={2}>
-          <Grid item xs={12}>
-            <Typography fontFamily='Playfair Display' sx={{ textAlign: 'right' }}>
-              I'm a full-stack software engineer with skills in Javascript (React), Python (Flask), HTML, and CSS. I'm passionate about learning and always looking to challenge myself. What excites me the most is using my skills to create innovative applications that make a positive impact. Let's connect and build something awesome together!
-            </Typography>
-          </Grid>
-        </Grid>
+        <Outlet />
       </Box>
     </Container>
   );
--- portfolio/src/components/homePage.js
+++ portfolio/src/components/homePage.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Container, Box, Grid, Typography, Link } from '@mui/material';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxWidth='lg'>
+      <Grid
+        className='flex-content' container spacing={2}>
+        <Grid item xs={12}>
+          <Typography fontFamily='Playfair Display' sx={{ textAlign: 'right' }}>
+            I'm a full-stack software engineer with skills in Javascript (React), Python (Flask), HTML, and CSS. I'm passionate about learning and always looking to challenge myself. What excites me the most is using my skills to create innovative applications that make a positive impact. Let's connect and build something awesome together!
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
