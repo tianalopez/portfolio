62f39cdec6f9a23a9519a2743e0fa784a6601daa
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-01-17 11:14:20 -0500

    adds animation styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -49,21 +49,7 @@ function App() {
           </Grid>
         </Grid>
         <Grid
-          className='flex-content'
-          container
-          spacing={2}
-          // sx={{
-          //   mb: 4,
-          //   mr: 4,
-          //   position: 'absolute',
-          //   bottom: 0,
-          //   right: 0,
-          //   textAlign: 'right',
-          //   justifyContent: 'flex-end',
-          //   alignItems: 'flex-end',
-          //   maxWidth: '25rem',
-          // }}
-        >
+          className='flex-content' container spacing={2}>
           <Grid item xs={12}>
             <Typography fontFamily='Playfair Display' sx={{ textAlign: 'right' }}>
               I'm a full-stack software engineer with skills in Javascript (React), Python (Flask), HTML, and CSS. I'm passionate about learning and always looking to challenge myself. What excites me the most is using my skills to create innovative applications that make a positive impact. Let's connect and build something awesome together!
