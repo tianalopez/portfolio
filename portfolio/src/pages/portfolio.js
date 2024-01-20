05cb6190feaba552d56e6c566ff8e65917b63792
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-01-20 11:56:14 -0500

    first attempt at popper

--- portfolio/src/pages/portfolio.js
+++ portfolio/src/pages/portfolio.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grid, Box, Typography, Link } from '@mui/material';
+import { Container, Grid, Box, Typography, Link, Popover } from '@mui/material';
 import "../styles/portfolio.css"
 
 const Portfolio = () => {
   const [projects, setProjects] = useState([]);
+  const [pop, setPop] = useState(false);
+  const [anchorPosition, setAnchorPosition] = useState({ top: 0, left: 0 });
 
   //fetch data from db.json
   useEffect(() => {
@@ -13,9 +15,22 @@ const Portfolio = () => {
       .catch((err) => alert(err));
   }, []);
 
+  const handlePopoverOpen = (event) => {
+    const { top, left, height } = event.currentTarget.getBoundingClientRect();
+    setAnchorPosition({ top: top + height, left });
+    setPop(true);
+  };
+
+  const handlePopoverClose = () => {
+    setPop(false);
+  };
+
   const visibleProjects = projects.map((project) => (
       <Grid key={project.id} className='proj-grid' item xs={12} sx={{width:'fit-content', textAlign:'right'}}>
-        <Link target='_blank' href={project.url} sx={{width: 'auto'}}className='proj-link'>
+        <Link
+          onMouseEnter={handlePopoverOpen}
+          onMouseLeave={handlePopoverClose}
+          target='_blank' href={project.url} sx={{width: 'auto'}}className='proj-link'>
           <Typography
           fontFamily='Playfair Display'
           fontWeight='200'
@@ -36,6 +51,21 @@ const Portfolio = () => {
           >
           {project.tech}
         </Typography>
+      <Popover
+        open={pop}
+        anchorPosition={anchorPosition}
+        onClose={handlePopoverClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        Test
+      </Popover>
       </Grid>
   ));
 
@@ -48,6 +78,7 @@ const Portfolio = () => {
           {visibleProjects}
         </Grid>
       </Box>
+
     </Container>
   );
 };
