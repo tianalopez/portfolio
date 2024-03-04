6562d41e4d8dbe1926d6a8961674a108d113cca9
lopez.y.tiana <lopez.y.tiana@gmail.com>
2024-03-04 18:28:30 +0000

    adds css edits

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,31 +10,31 @@ const Contact = () => {
 
   return (
     <Container maxWidth='lg'>
-      <Box>
-        <Grid container spacing={2} sx={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
-          <span className='me'>
-            <img className='img-me' alt='Tiana holding baby goat' src='/images/me.png'></img>
-          </span>
-        </Grid>
-        <Grid className='contact-me' container spacing={2} sx={{ width: 'fit-content', display: 'flex', flexDirection: 'column' }}>
-          <Link target='_blank' href={'https://www.linkedin.com/in/tiana-lopez-728863180/'} className='contact-link'>
-              LinkedIn
-              <CallMadeOutlinedIcon sx={{ color: '#232F2A', ml: 0.5 }}/>
-          </Link>
-          <Link target='_blank' href={'https://github.com/tianalopez'} className='contact-link'>
-              Github
-              <CallMadeOutlinedIcon sx={{ color: '#232F2A', ml: 0.5 }}/>
-          </Link>
-          <Link target='_blank' href={'https://tianalopez.hashnode.dev/'} className='contact-link'>
-              Blog
-              <CallMadeOutlinedIcon sx={{ color: '#232F2A', ml: 0.5 }}/>
-          </Link>
-          <Typography sx={{ ml: 1.5 }}>•</Typography>
-          <Link className='contact-link' href={`mailto:${email}`}>
-            {email}
-            <CallMadeOutlinedIcon sx={{ color: '#232F2A', ml: 0.5 }} />
-          </Link>
-        </Grid>
+      <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
+          <Grid container spacing={2} sx={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
+            <span className='me'>
+              <img className='img-me' alt='Tiana holding baby goat' src='/images/me.png'></img>
+            </span>
+          </Grid>
+          <Grid className='contact-me' container spacing={2} sx={{ width: 'fit-content', display: 'flex', flexDirection: 'column' }}>
+            <Link target='_blank' href={'https://www.linkedin.com/in/tiana-lopez-728863180/'} className='contact-link'>
+                LinkedIn
+                <CallMadeOutlinedIcon sx={{ color: '#232F2A', ml: 0.5 }}/>
+            </Link>
+            <Link target='_blank' href={'https://github.com/tianalopez'} className='contact-link'>
+                Github
+                <CallMadeOutlinedIcon sx={{ color: '#232F2A', ml: 0.5 }}/>
+            </Link>
+            <Link target='_blank' href={'https://tianalopez.hashnode.dev/'} className='contact-link'>
+                Blog
+                <CallMadeOutlinedIcon sx={{ color: '#232F2A', ml: 0.5 }}/>
+            </Link>
+            <Typography sx={{ ml: 1.5 }}>•</Typography>
+            <Link className='contact-link' href={`mailto:${email}`}>
+              {email}
+              <CallMadeOutlinedIcon sx={{ color: '#232F2A', ml: 0.5 }} />
+            </Link>
+          </Grid>
       </Box>
     </Container>
   );
